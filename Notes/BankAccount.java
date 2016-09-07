65bacc2b3edaebfefa9225702d875bbabbb68c4e
Geoff Schmit <gcschmit@gmail.com>
2016-09-07 14:24:03 -0500

    chapter 3 notes
    created instance variables
    created first method
    in the middle of writing javadoc documentation

---
+++ Notes/BankAccount.java
@@ -0,0 +1,43 @@
+
+/**
+ * This class models a bank account.
+ * 
+ * @author gcschmit
+ * @version 7 September 2016
+ */
+public class BankAccount
+{
+    /*
+     * Instance variables store the object's properties
+     *      specify the visibility (e.g., private)
+     *          public: accessible by anyone
+     *          private: only accessible by methods in this class
+     *      specify the type (e.g., double)
+     *      specifiy the name (e.g., balance)
+     *  
+     *  Instance variables differ from local variables in the following ways:
+     *      scoped to their object (accessible from all methods)
+     *      automatically initialized to a default value (0, false, null)
+     */
+    private double balance;
+
+    /**
+     * Constructor for objects of class BankAccount
+     */
+    public BankAccount()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * Deposits the specified amount of money in this bank account
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public void deposit( double amount )
+    {
+        
+    }
+}
