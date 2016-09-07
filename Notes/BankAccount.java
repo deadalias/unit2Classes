7cab180839e0225115e360c72c589c2a623233bd
Geoff Schmit <gcschmit@gmail.com>
2016-09-07 13:00:11 -0500

    chapter 3 notes
    need to create additional methods
    did introduce Javadoc

---
+++ Notes/BankAccount.java
@@ -0,0 +1,49 @@
+
+/**
+ * This class models an account at a bank.
+ * 
+ * @author gcschmit
+ * @version 07sep2016
+ */
+public class BankAccount
+{
+    /* 
+     * instance variable
+     *      stores the object's properties
+     *      specify the visibility (e.g., private)
+     *          public: accessible by anyone
+     *          private: only accessible by methods in the same class
+     *      specify the type (e.g., double)
+     *      specify the name (e.g., balance)
+     *      
+     *      different than local variables in the following ways:
+     *          scope to the object (accessible from all methods in class)
+     *          automatically initialized to a default value (0, false, null)
+     */
+    private double balance;
+
+    /**
+     * Constructor for objects of class BankAccount
+     */
+    public BankAccount()
+    {
+        // initialise instance variables
+        
+    }
+
+    /**
+     * Deposits money into the bank account.
+     * 
+     * Declaring methods:
+     *      specify the visibility (e.g., public)
+     *      specify the return type (e.g., void)
+     *      specify the method name (e.g., deposit)
+     *      specify the parameters and their types (e.g., amount of type double)
+     * 
+     * @param  amount   the amount to deposit in the account
+     */
+    public void deposit( double amount )
+    {
+        
+    }
+}
