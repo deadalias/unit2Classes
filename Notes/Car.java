6b8c90104aa0d8e978ef3385995ed57a0907658d
Geoff Schmit <gcschmit@gmail.com>
2016-09-09 14:14:34 -0500

    Outlined the Car class; started the CarTest class
    need to write two more test methods.

---
+++ Notes/Car.java
@@ -0,0 +1,52 @@
+
+/**
+ * This class models a car that can be driven and consumes fuel.
+ * 
+ * @author gcschmit
+ * @version 9 September 2016
+ */
+public class Car
+{
+    private double fuelEfficiency; // in units of miles per gallon
+    private double fuelInTank; // in units of gallons
+
+    /**
+     * Creates a car with the specified fuel efficiency
+     * 
+     * @param   efficiency  the fuel efficiency, in miles per gallon, of this new car
+     */
+    public Car( double efficiency )
+    {
+    }
+
+    /**
+     * Drives this car the specified distance, consuming fuel
+     * 
+     * @param  distance   the distance, in miles, this car drives
+     */
+    public void drive( double distance )
+    {
+    }
+    
+    /**
+     * Adds the specified amount of fuel to this car's tank
+     *
+     * @param  amount   the amount of fuel, in gallons, to add to this car's tank
+     */
+    public void addGas( double amount )
+    {
+    }
+    
+    /**
+     * Returns the amount of fuel in this car's tank
+     *
+     * @return     the amount of fuel, in gallons, in this car's tank
+     */
+    public double getGasInTank()
+    {
+        return 0;
+    }
+
+
+    
+}
